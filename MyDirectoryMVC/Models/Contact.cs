ad95a1ae939619e6f9ee16f4fc0f3fb6ef318274
Keila Valdez <keyvapers@gmail.com>
2022-08-18 01:09:18 -0500

    Add Contact Model

---
+++ MyDirectoryMVC/Models/Contact.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace MyDirectoryMVC.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Nombre(s) es obligatorio")]
+        [Display(Name = "Nombre(s)")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Apellidos es obligatorio")]
+        [Display(Name = "Apellidos")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Teléfono es obligatorio")]
+        [Display(Name = "Teléfono")]
+        public string Telephone { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Correo es obligatorio")]
+        [Display(Name = "Correo")]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Correo no válido")]
+        public string Email { get; set; } = string.Empty;
+    }
+}
