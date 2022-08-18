30aa105d34cdaba31390064c4ba3ac1a8598c00c
Keila Valdez <keyvapers@gmail.com>
2022-08-18 01:26:57 -0500

    Add seedData

---
+++ MyDirectoryMVC/Models/SeedData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyDirectoryMVC.Data;
+
+namespace MyDirectoryMVC.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MyDirectoryMVCContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MyDirectoryMVCContext>>()))
+            {
+                
+                if (context.Contact.Any())
+                {
+                    return;   
+                }
+
+                context.Contact.AddRange(
+                    new Contact
+                    {
+                        Name = "Juan",
+                        LastName = "Ochoa",
+                        Telephone = "3311144525",
+                        Email = "juan.ochoa@gmail.com"
+                    },
+
+                    new Contact
+                    {
+                        Name = "Laura",
+                        LastName = "Herrera",
+                        Telephone = "3542144424",
+                        Email = "laura.herrera@gmail.com"
+                    },
+
+                    new Contact
+                    {
+                        Name = "Omar",
+                        LastName = "Aguilar",
+                        Telephone = "5552451025",
+                        Email = "omar.aguilar@gmail.com"
+                    },
+
+                    new Contact
+                    {
+                        Name = "Alejandra",
+                        LastName = "Miranda",
+                        Telephone = "4521452008",
+                        Email = "ale.miranda@gmail.com"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- MyDirectoryMVC/Program.cs
+++ MyDirectoryMVC/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MyDirectoryMVC.Data;
+using MyDirectoryMVC.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MyDirectoryMVCContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MyDirectoryMVCContext") ?? throw new InvalidOperationException("Connection string 'MyDirectoryMVCContext' not found.")));
@@ -9,6 +10,13 @@ builder.Services.AddDbContext<MyDirectoryMVCContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
